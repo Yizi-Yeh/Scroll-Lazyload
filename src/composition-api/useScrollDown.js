91f183e8e73b44cba79081e43e3aac78c6d75b39
Yizi-Yeh <g384357@gmail.com>
2021-09-26 11:07:40 +0800

    feat: scroll事件中加入節流（throttle）

--- src/composition-api/useScrollDown.js
+++ src/composition-api/useScrollDown.js
@@ -1,17 +1,19 @@
 // 檢測捲軸是否到底
-import { onMounted ,onUnmounted,ref} from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
+import { throttle } from "../lib/Throttle.js";
 
 export function useScrollDown() {
   const isDown = ref(false);
 
-  const handleScrollDown = () => {
+  const handleScrollDown = throttle(() => {
+      console.log('scroll')
     const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
     if (scrollTop + clientHeight >= scrollHeight) {
-        isDown.value= true
-    }else{
-        isDown.value= false
+      isDown.value = true;
+    } else {
+      isDown.value = false;
     }
-  };
+  },300);
 
   onMounted(() => {
     window.addEventListener("scroll", handleScrollDown);
